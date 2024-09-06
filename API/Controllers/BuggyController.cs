3b2fa34e7b26503b7cdc4f5d604d836665b0d1cc
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-09-06 22:30:07 +0300

    create middleware for exceptions

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -6,29 +6,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class BuggyController :BaseApiController
+    public class BuggyController : BaseApiController
     {
         [HttpGet("not-found")]
-        public ActionResult GetNotFound(){
+        public ActionResult GetNotFound()
+        {
             return NotFound();
         }
         [HttpGet("bad-request")]
-        public ActionResult GetBadRequest(){
-            return BadRequest(new ProblemDetails{Title="This is a bad request"});
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ProblemDetails { Title = "This is a bad request" });
         }
         [HttpGet("unauthorized")]
-        public ActionResult GetUnauthorized(){
+        public ActionResult GetUnauthorized()
+        {
             return Unauthorized();
         }
         [HttpGet("validation-error")]
-        public ActionResult GetValidationError(){
+        public ActionResult GetValidationError()
+        {
             ModelState.AddModelError("Problem1", "this is the first error");
             ModelState.AddModelError("Problem2", "this is the second error");
             return ValidationProblem();
         }
         [HttpGet("server-error")]
-        public ActionResult GetServerError(){
-            throw new Exception("This is a server error");
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");  //burada kullanıcının karşısına bir sürü hata yazısı çıkar bu doğru değil bunu önlemek için ExceptionMiddleware yazdık.
         }
 
     }
